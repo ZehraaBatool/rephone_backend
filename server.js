87f1736511116eac4088fafd59355c2921707da9
Zehra <zehra.s2832@gmail.com>
2025-05-11 18:09:48 +0500

    Admin Url

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors({
-    origin: ['http://localhost:5173','http://localhost:3000'],
+    origin: ['http://localhost:5173','https://rephone-admin.vercel.app'],
     credentials: true,
 }));
 
