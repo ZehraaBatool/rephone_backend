4f5eda6ed806809fdbfd8ff9364d90175cea0160
Zehra Batool <121621338+ZehraaBatool@users.noreply.github.com>
2025-07-29 13:48:32 +0500

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,6 @@
 import pool from "../db/connectDB.js";
 import { validationResult } from "express-validator";
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcrypt.js';
 
 // upload phone
 const uploadPhone = async (req, res) => {
